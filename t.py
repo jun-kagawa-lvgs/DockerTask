70401cfedd94f9216f16b1e9817aa40ad9b6b9d3
jun-kagawa-lvgs <jun.kagawa@leverages.jp>
2022-09-20 14:33:08 +0900

    add: github actions

---
+++ t.py
@@ -0,0 +1,28 @@
+
+
+
+from re import T
+
+
+t = (1, 4)
+k = (1, 2)
+
+def main(n, m):
+    t = m // 4
+    k = m % 4
+
+    return t, k
+
+ 
+
+
+if __name__ == '__main__':
+    print(main(3, 8))
+        
+
+
+
+
+
+
+
